1b0ac952c198330201c825adbc7cfb77aa7a1766
Philipp Henkel <philipp.henkel@gmail.com>
2018-06-12 00:40:10 +0200

    Removed obsolete function

--- mp3.cpp
+++ mp3.cpp
@@ -14,17 +14,6 @@ namespace makerbit {
       uBit.serial.read(MicroBitSerialMode::ASYNC);
     }
 
-    //% 
-    void readResponse(int tx, int rx, int baud) {
-      MicroBitPin* txp = getPin((int)tx); if (!txp) return;
-      MicroBitPin* rxp = getPin((int)rx); if (!rxp) return;
-      uBit.serial.redirect(txp->name, rxp->name);
-      uBit.serial.baud(baud);
-
-      // Enforce initialization of serial RX buffers to prevent hang
-      uBit.serial.read(MicroBitSerialMode::ASYNC);
-    }
-
     //%
     int readSerialToBuffer(Buffer buffer) {
       return uBit.serial.read(buffer->payload, buffer->length);
