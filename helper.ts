7bedf6288100f72213f9bdb4eff40bde0ff7b768
Philipp Henkel <philipp.henkel@gmail.com>
2018-10-17 22:44:05 +0200

    Fix level conversation so that switching between blocks and JS works properly

--- helper.ts
+++ helper.ts
@@ -17,24 +17,24 @@ namespace makerbit {
     //% block="set all LED pins to %level=makerbit_helper_level"
     //% weight=90
     export function setLedPins(level: number): void {
-        for (let i=5; i<=16; i++) {
+        for (let i = 5; i <= 16; i++) {
             setDigitalPin(i, level)
         }
-     }
+    }
 
-     /**
-      * Sets a digital pin to either on or off.
-      * Configures this pin as a digital output (if necessary).
-      * @param name name of the pin in the range from 0 to 20, eg: 5
-      * @param level digital pin level, either 0 or 1
-      */
-     //% blockId="makerbit_helper_set_digital_pin"
-     //% block="set digital pin %pin | to %level=makerbit_helper_level"
-     //% name.min=0 name.max=20
-     //% weight=89
-     export function setDigitalPin(name: number, level: number): void {
+    /**
+     * Sets a digital pin to either on or off.
+     * Configures this pin as a digital output (if necessary).
+     * @param name name of the pin in the range from 0 to 20, eg: 5
+     * @param level digital pin level, either 0 or 1
+     */
+    //% blockId="makerbit_helper_set_digital_pin"
+    //% block="set digital pin %pin | to %level=makerbit_helper_level"
+    //% name.min=0 name.max=20
+    //% weight=89
+    export function setDigitalPin(name: number, level: number): void {
         pins.digitalWritePin(name + DigitalPin.P0, level)
-     }
+    }
 
     /**
       * Sets an analog pin to a given level.
@@ -44,14 +44,14 @@ namespace makerbit {
       * @param name name of the pin in the range from 0 to 20, eg: 5
       * @param level value in the range from 0 to 1023 eg: 1023
       */
-     //% blockId="makerbit_helper_set_analog_pin"
-     //% block="set analog pin %pin | to %level"
-     //% name.min=0 name.max=20
-     //% level.min=0 level.max=1023
-     //% weight=88
-     export function setAnalogPin(name: number, level: number): void {
+    //% blockId="makerbit_helper_set_analog_pin"
+    //% block="set analog pin %pin | to %level"
+    //% name.min=0 name.max=20
+    //% level.min=0 level.max=1023
+    //% weight=88
+    export function setAnalogPin(name: number, level: number): void {
         pins.analogWritePin(name + AnalogPin.P0, level)
-     }
+    }
 
     /**
      * Turns a digital pin level into a number.
@@ -60,7 +60,8 @@ namespace makerbit {
     //% weight=49
     //% blockId=makerbit_helper_level
     //% block="level %level"
-    export function level(level: PinLevel): number {
+    export function level(level?: PinLevel): number {
+        if (level === undefined) return PinLevel.High
         return level
     }
 }
