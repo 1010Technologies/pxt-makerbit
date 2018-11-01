640bffa3be1315e6fb6c0fb71c628c830e95d52f
Philipp Henkel <philipp.henkel@gmail.com>
2018-11-01 23:28:02 +0100

    Increase stability of ultrasonic measurements

--- ultrasonic.ts
+++ ultrasonic.ts
@@ -2,15 +2,18 @@
 
 const enum DistanceUnit {
     //% block="cm"
-    CM = 10000,
+    CM = 58,       // Duration of echo round-trip in Microseconds (uS) for two centimeters, 343 m/s at sea level and 20°C
     //% block="inch"
-    INCH = 25400
+    INCH = 148     // Duration of echo round-trip in Microseconds (uS) for two inches, 343 m/s at sea level and 20°C
 }
 
 namespace makerbit {
 
+    const MAX_ULTRASONIC_TRAVEL_TIME = 300 * DistanceUnit.CM
+
     /**
-     * Measures the distance and returns the result in a range from 1 to 300 centimeters or up to 118 inch. The maximum value is returned to indicate when no object was detected.
+     * Measures the distance and returns the result in a range from 1 to 300 centimeters or up to 118 inch.
+     * The maximum value is returned to indicate when no object was detected.
      * @param unit unit of distance, eg: DistanceUnit.CM
      * @param trig pin connected to trig, eg: MakerBitPin.P5
      * @param echo Pin connected to echo, eg: MakerBitPin.P8
@@ -23,31 +26,40 @@ namespace makerbit {
     //% echo.fieldOptions.tooltips="false"
     //% weight=45
     export function getUltrasonicDistance(unit: DistanceUnit, trig: MakerBitPin, echo: MakerBitPin): number {
-        const trigPinNumber: number = trig
-        const echoPinNumber: number = echo
-
-        const MAX_DIST_MICROMETER = 3000 * 1000
-        const VELOCITY_OF_SOUND = 343  // 343 m/s at sea level and 20°C
-        const MAX_PULSE_DURATION_US = Math.idiv(2 * MAX_DIST_MICROMETER, VELOCITY_OF_SOUND)
-
-        // Trigger pulse
-        pins.setPull(trigPinNumber, PinPullMode.PullNone)
-        pins.digitalWritePin(trigPinNumber, 0)
-        control.waitMicros(2)
-        pins.digitalWritePin(trigPinNumber, 1)
-        control.waitMicros(10)
-        pins.digitalWritePin(trigPinNumber, 0)
-
-        // Receive echo
-        const pulseDuration = pins.pulseIn(echoPinNumber, PulseValue.High, MAX_PULSE_DURATION_US)
-        let objectDistance = Math.idiv(pulseDuration * VELOCITY_OF_SOUND, 2)
-
-        // Map timeouts to max distance and clip at max distance
-        if (objectDistance === 0 || objectDistance > MAX_DIST_MICROMETER) {
-            objectDistance = MAX_DIST_MICROMETER
-        }
-
-        return Math.idiv(objectDistance, unit)
+        const travelTime = medianTravelTime(() => ping(trig, echo))
+        return Math.idiv(travelTime, unit)
     }
 
+    function medianTravelTime(measure: () => number) {
+        const travelTimes: number[] = []
+
+        for (let i = 0; i < 3; i++) {
+            basic.pause(25) // wait for echos to disappear
+            const travelTime = measure()
+            if (travelTime > 0) {
+                travelTimes.push(travelTime)
+            }
+        }
+
+        travelTimes.sort()
+
+        return travelTimes.length > 0
+            ? travelTimes[travelTimes.length >> 1]
+            : MAX_ULTRASONIC_TRAVEL_TIME
+    }
+
+    function ping(trig: MakerBitPin, echo: MakerBitPin): number {
+        // Reset trigger pin
+        pins.setPull(trig as number, PinPullMode.PullNone)
+        pins.digitalWritePin(trig as number, 0)
+        control.waitMicros(2)
+
+        // Trigger pulse
+        pins.digitalWritePin(trig as number, 1)
+        control.waitMicros(10)
+        pins.digitalWritePin(trig as number, 0)
+
+        // Measure travel time of echo
+        return pins.pulseIn(echo as number, PulseValue.High, MAX_ULTRASONIC_TRAVEL_TIME)
+    }
 }
