4058956bf8e4c652ae8e1e110a3a8818e25276ee
Philipp Henkel <philipp.henkel@gmail.com>
2019-01-23 22:06:43 +0100

    Move MP3 into separate extension

--- test.ts
+++ test.ts
@@ -2,70 +2,42 @@
  * Helper tests
  */
 
-makerbit.setLedPins(0)
-makerbit.setDigitalPin(5, 1)
-makerbit.setAnalogPin(5, 1023)
-let level: number = makerbit.level(PinLevel.High)
-
-
-/**
- * Serial MP3 tests
- */
-
-makerbit.connectSerialMp3(MakerBitPin.A0, MakerBitPin.A1)
-
-makerbit.playMp3TrackFromFolder(1, 1, Mp3Repeat.No)
-makerbit.playMp3TrackFromFolder(1, 1, Mp3Repeat.Forever)
-makerbit.playMp3Folder(1, Mp3Repeat.No)
-makerbit.playMp3Folder(1, Mp3Repeat.Forever)
-makerbit.setMp3Volume(30)
-
-makerbit.runMp3Command(Mp3Command.PLAY_NEXT_TRACK)
-makerbit.runMp3Command(Mp3Command.PLAY_PREVIOUS_TRACK)
-makerbit.runMp3Command(Mp3Command.INCREASE_VOLUME)
-makerbit.runMp3Command(Mp3Command.DECREASE_VOLUME)
-makerbit.runMp3Command(Mp3Command.PAUSE)
-makerbit.runMp3Command(Mp3Command.RESUME)
-makerbit.runMp3Command(Mp3Command.STOP)
-makerbit.runMp3Command(Mp3Command.MUTE)
-makerbit.runMp3Command(Mp3Command.UNMUTE)
-
-makerbit.onMp3TrackStarted(() => { })
-makerbit.onMp3TrackCompleted(() => { })
-const folder: number = makerbit.mp3Folder()
-const track: number = makerbit.mp3Track()
-const volume: number = makerbit.mp3Volume()
-
+makerbit.setLedPins(0);
+makerbit.setDigitalPin(5, 1);
+makerbit.setAnalogPin(5, 1023);
+let level: number = makerbit.level(PinLevel.High);
 
 /**
  * Ultrasonic tests
  */
 
-const distance: number = makerbit.getUltrasonicDistance(DistanceUnit.CM, MakerBitPin.P5, MakerBitPin.P8)
-
+const distance: number = makerbit.getUltrasonicDistance(
+  DistanceUnit.CM,
+  MakerBitPin.P5,
+  MakerBitPin.P8
+);
 
 /**
  * LCD tests
  */
 
-makerbit.connectLcd(39)
-makerbit.showStringOnLcd("Hello world", 0, 15)
-makerbit.showNumberOnLcd(42, 16, 2)
-makerbit.clearLcd()
-makerbit.setLcdBacklight(LcdBacklight.On)
-const pos: number = makerbit.position(LcdPosition.P0)
-const isLcdConnected: boolean = makerbit.isLcdConnected()
-
+makerbit.connectLcd(39);
+makerbit.showStringOnLcd("Hello world", 0, 15);
+makerbit.showNumberOnLcd(42, 16, 2);
+makerbit.clearLcd();
+makerbit.setLcdBacklight(LcdBacklight.On);
+const pos: number = makerbit.position(LcdPosition.P0);
+const isLcdConnected: boolean = makerbit.isLcdConnected();
 
 /**
  * IR tests
  */
 
-makerbit.connectInfrared(MakerBitPin.A0)
-makerbit.onIrButtonPressed(IrButton.Ok, () => { })
-makerbit.onIrButtonReleased(IrButton.Up, () => { })
-makerbit.isIrButtonPressed(IrButton.Number_0)
-makerbit.onIrCommandReceived(() => { })
-makerbit.onIrCommandExpired(() => { })
-const command: number = makerbit.irCommandCode()
-const button: number = makerbit.irButton(IrButton.Number_9)
+makerbit.connectInfrared(MakerBitPin.A0);
+makerbit.onIrButtonPressed(IrButton.Ok, () => {});
+makerbit.onIrButtonReleased(IrButton.Up, () => {});
+makerbit.isIrButtonPressed(IrButton.Number_0);
+makerbit.onIrCommandReceived(() => {});
+makerbit.onIrCommandExpired(() => {});
+const command: number = makerbit.irCommandCode();
+const button: number = makerbit.irButton(IrButton.Number_9);
