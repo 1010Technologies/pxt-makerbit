0d16d6c59947d83b40d82d98bf922b56eb2c6bc6
Philipp Henkel <philipphenkel@users.noreply.github.com>
2020-08-29 12:21:10 +0200

    Establish explicit order the subcategories

--- namespace.ts
+++ namespace.ts
@@ -3,5 +3,6 @@
  */
 //% color=#0fbc11 icon="\u272a" block="MakerBit"
 //% category="MakerBit"
+//% subcategories='["Pins", "Touch", "LCD", "MP3", "IR Receiver", "IR Sender", "Ultrasonic", "Motors"]'
 namespace makerbit {
 }
