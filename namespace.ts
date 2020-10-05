f90d2ddd882a29e8d151306a36d9f38130f6ae40
Philipp Henkel <philipphenkel@users.noreply.github.com>
2020-10-05 08:23:05 +0200

    Update list of subcategories to include LCD1602

--- namespace.ts
+++ namespace.ts
@@ -3,6 +3,6 @@
  */
 //% color=#0fbc11 icon="\u272a" block="MakerBit"
 //% category="MakerBit"
-//% subcategories='["Pins", "Touch", "LCD", "MP3", "IR Sender", "IR Receiver", "Ultrasonic", "Motors"]'
+//% subcategories='["Pins", "Touch", "LCD1602", "MP3", "IR Sender", "IR Receiver", "Ultrasonic", "Motors"]'
 namespace makerbit {
 }
