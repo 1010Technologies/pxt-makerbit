7cdbb2d945e88ad1cd2b6ba5628e3f414a36c6c0
Philipp Henkel <philipphenkel@users.noreply.github.com>
2021-05-04 20:46:35 +0200

    Remove sub category order and reduce extensions to the core

--- namespace.ts
+++ namespace.ts
@@ -3,6 +3,5 @@
  */
 //% color=#0fbc11 icon="\u272a" block="MakerBit"
 //% category="MakerBit"
-//% subcategories='["Pins", "Touch", "LCD1602", "MP3", "IR Sender", "IR Receiver", "Ultrasonic", "Motors", "Zoom"]'
 namespace makerbit {
 }
